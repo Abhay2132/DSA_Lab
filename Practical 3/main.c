0ec3601282c7a362cda203d9644679da8ecc3548
Abhay2132 <84978755+Abhay2132@users.noreply.github.com>
2023-09-27 06:49:18 +0000

    gg

--- Practical 3/main.c
+++ Practical 3/main.c
@@ -3,22 +3,29 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int * matrix1, *matrix2, row1, col1, row2, col2;
+int * matrix1, *matrix2, row1=0, col1=0, row2=0, col2=0;
 
 void create();
 void add();
 void mulitply();
 void transpose();
 void isSparse(int matrixN);
-int get(int targetN, int row, int col);
-int set(int targetN, int row, int col, int value);
+int get(int * , int , int , int);
+void set(int * , int , int , int , int );
+
 
 int main(int argc, char *argv[]) {
 	
 	int ch=9;
 	do{
 		printf("\t\t Matrix OPs \n");
-		printf("Choose an option :- \n\n");
+
+		printf(" MATRIX 1 :- \n");
+		printMatrix(matrix1, row1, col1);
+		printf("\n MATRIX2 :- \n");
+		printMatrix(matrix2, row2, col2);
+
+		printf("\n\n Choose an option :- \n\n");
 		
 		printf(" 1.) Create Matrices\n");
 		printf(" 2.) Add Matrices\n");
@@ -44,8 +51,36 @@ int main(int argc, char *argv[]) {
 		return 0;
 }
 
+
+void printMatrix(int * matrix, int row, int col){
+	for(int i=0; i<row; i++){
+		for(int j=0; j<col; j++){
+			printf("%d ", get(matrix, i,j, row));
+		}
+		printf("\n");
+	}
+}
+
+int get(int * matrix, int row, int col, int rowSize){
+	return *(matrix + rowSize*row + col);
+}
+
+void set(int * matrix, int row, int col , int rowSize, int value){
+	*(matrix + rowSize * row + col) = value;	
+}
+
 void create(){
 	printf("\n Matrix1 :- \n");
-	printf(" row : ");
-	scanf("%d", &row1);
+	printf(" row and column size : ");
+	scanf("%d %d", &row1, &col1);
+
+	matrix1 = (int *) malloc(sizeof(int)*(row1*col1));
+	for(int i=0; i<row1; i++){
+		for(int j=0; j< col1; j++){\
+			int value;
+			scanf("%d", value);
+			set(matrix1, i,j, row1, value);
+		}
+	}
+
 }
