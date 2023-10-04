8fde8b2eacef76205575fadfb670c700e2d2ded6
Abhay2132 <84978755+Abhay2132@users.noreply.github.com>
2023-10-04 06:50:16 +0000

    hoho

--- Practical 5/main.c
+++ Practical 5/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "helper.h"
 #include "models.h"
-
-
+#include "methods.h"
 
 int main()
 {
@@ -61,58 +60,6 @@ int main()
 
         return 0;
     }
-}
-
-void printHeader()
-{
-    printf(" USN\t\tname\t\tbranch\t\tsem\t\tPhone");
-}
-
-void printRow(struct data *)
-{
-
-    printf(" %d", data->usn);
-    s(8 - numLen(data->usn));
-    printf("%s", data->name);
-    s(8 - strLen(data->name));
-    printf("%s", data->branch);
-    s(8 - strLen(data->branch));
-    printf("%d", sem);
-    s(8 - strLen(data->sem));
-    printf("%d", data->phNo);
-
-    printf("")
-}
-
-void printRecords(struct node *start, int limit)
-{
-    int count = 0;
-    struct node *tmp = start;
-
-    while (tmp->link != NULL)
-    {
-        if (count++ >= limit)
-        {
-            printf("\n . . .");
-            break;
-        }
-        printRow(tmp->data);
-        printf("\n");
-        tmp = tmp->link;
-
-        count++;
-    }
-}
-
-struct node *insert_end(struct node * start){
-    struct data * newData;
-    struct node * tmp = start;
-
-    printf("\n\t\t Insert at End\n");
-    printf("\n ")
-
-    if(start == NULL)
-    while()
 }
 
 // Do not type after this line
--- Practical 5/methods.h
+++ Practical 5/methods.h
@@ -19,3 +19,59 @@ struct node *data_at(struct node *);
 struct node *append(struct node *);
 
 void printRecord(int n);
+
+
+void printHeader()
+{
+    printf(" USN\t\tname\t\tbranch\t\tsem\t\tPhone");
+}
+
+void printRow(struct data *)
+{
+
+    printf(" %d", data->usn);
+    s(8 - numLen(data->usn));
+    printf("%s", data->name);
+    s(8 - strLen(data->name));
+    printf("%s", data->branch);
+    s(8 - strLen(data->branch));
+    printf("%d", sem);
+    s(8 - strLen(data->sem));
+    printf("%d", data->phNo);
+
+    printf("")
+}
+
+void printRecords(struct node *start, int limit)
+{
+    int count = 0;
+    struct node *tmp = start;
+
+    while (tmp->link != NULL)
+    {
+        if (count++ >= limit)
+        {
+            printf("\n . . .");
+            break;
+        }
+        printRow(tmp->data);
+        printf("\n");
+        tmp = tmp->link;
+
+        count++;
+    }
+}
+
+struct node *insert_end(struct node * start){
+    struct data * newData;
+    struct node * tmp = start;
+
+    printf("\n\t\t Insert at End\n");
+    printf("\n ")
+
+    if(start == NULL)
+    while()
+}
+
+
+// Do not type after this line
